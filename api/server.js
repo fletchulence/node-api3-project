d7e13673c99448795c8416164c15a9bed81ff297
david fletcher <davidfletcheraaron@gmail.com>
2022-01-12 11:58:57 -0500

    initial setup done

--- api/server.js
+++ api/server.js
@@ -6,6 +6,10 @@ const server = express();
 server.use(express.json())
 
 // global middlewares and the user's router need to be connected here
+//! require usersRouter
+const usersRouter = require('./users/users-router');
+// use Router
+server.use('/api/users', usersRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
